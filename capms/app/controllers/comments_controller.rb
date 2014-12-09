50ec64a33102317f33ef1b156d7e053d91c89b7b
Daniel Rodenberg <dsr8wf@mail.missouri.edu>
2014-12-08 20:52:33 -0600

    comments

--- capms/app/controllers/comments_controller.rb
+++ capms/app/controllers/comments_controller.rb
@@ -4,15 +4,19 @@ class CommentsController < ApplicationController
     	@message = Message.find(params[:message_id])
    		@comment = @message.comments.create(comment_params)
       @allUsers = User.all
+      #loop through all users
       @allUsers.each do |u|
+        #if user id is not equal to current user id then make a new bulletin
         if u.id != current_user.id
           Bulletin.new(user_id: u.id, message_id: @message.id).save
         end
       end
+      #redirect to the correct message
    	 	redirect_to message_path(@message)
   	end
 
   	def destroy
+      #find the message params and the find the comment params and destroy comment
 	    @message = Message.find(params[:message_id])
 	    @comment = @message.comments.find(params[:id])
 	    @comment.destroy
--- capms/app/models/message.rb
+++ capms/app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ActiveRecord::Base
+	#messages have many comments and are destroying depends on confirmation
 	has_many :comments, dependent: :destroy
+	#the title must be at least 5 characters
 	validates :title, presence: true,
                     length: { minimum: 5 }
 end
